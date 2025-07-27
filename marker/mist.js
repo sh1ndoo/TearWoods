321b016eee255a5cf598ae658411f3589d937739
Дмитрий <154452992+sh1ndoo@users.noreply.github.com>
2025-07-27 22:07:20 +0700

    Create mist.js

---
+++ marker/mist.js
@@ -0,0 +1,47 @@
+var mist = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "01",
+                "description": "Mist Invasion Time: 11:00 13:00 18:00 22:00 00:00"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    40.5,
+                    -151
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "02",
+                "description": "Mist Invasion Time: 11:00 13:00 18:00 22:00 00:00"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    124.25,
+                    -143.5
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "03",
+                "description": "Mist Invasion Time: 11:00 13:00 18:00 22:00 00:00"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    192,
+                    -160.5
+                ]
+            }
+        }
+    ]
+}
