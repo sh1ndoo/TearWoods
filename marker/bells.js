5f4309298f168a3af43b2742e7c5b8189b34766d
Дмитрий <154452992+sh1ndoo@users.noreply.github.com>
2025-08-05 16:43:05 +0700

    Create bells.js

---
+++ marker/bells.js
@@ -0,0 +1,47 @@
+var bells = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "02",
+                "description": "."
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    148.53125,
+                    -97.3125
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "01",
+                "description": "."
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    93.75,
+                    -78
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "03",
+                "description": "."
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    97.625,
+                    -230.375
+                ]
+            }
+        }
+    ]
+}
